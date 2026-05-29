0dfea1d82eee29e73bb7a8f9a8f0831c0a895000
Rewindz <hx3er5now@gmail.com>
2026-05-29 00:34:28 -0400

    change STATUS enum names to avoid collisons, fix formatting

--- include/rz/defs/defs.hpp
+++ include/rz/defs/defs.hpp
@@ -1,25 +1,22 @@
 #pragma once
 
-#include <functional>
 #include <expected>
+#include <functional>
 #include <memory>
 #include <string>
 
-namespace rz
-{
-    enum class STATUS
-    {
-        ERROR = 0,
-        SUCCESS,
-    };
+namespace rz {
+enum class STATUS {
+  RZ_ERROR = 0,
+  RZ_SUCCESS,
+};
 
-    using logCB = std::function<void(const std::string&)>;
-    inline constexpr auto emptyCB = [](const std::string&) {};
+using logCB = std::function<void(const std::string &)>;
+inline constexpr auto emptyCB = [](const std::string &) {};
 
-    template <typename T>
-    using EXPECTED_UPTR = std::expected<std::unique_ptr<T>, STATUS>;
+template <typename T>
+using EXPECTED_UPTR = std::expected<std::unique_ptr<T>, STATUS>;
 
-    template <typename T>
-    using EXPECTED_OBJ = std::expected<T, STATUS>;
+template <typename T> using EXPECTED_OBJ = std::expected<T, STATUS>;
 
 } // namespace rz
