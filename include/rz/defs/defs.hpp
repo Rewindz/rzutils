f5bd6c72fb26adfcfaf64412b69af4ee44793aae
Rewindz <hx3er5now@gmail.com>
2026-07-18 23:23:38 -0400

    add new logger presets for rz::logCB

--- include/rz/defs/defs.hpp
+++ include/rz/defs/defs.hpp
@@ -3,7 +3,9 @@
 #include <expected>
 #include <functional>
 #include <memory>
-#include <string>
+#include <string_view>
+#include <print>
+#include <cstdio>
 
 namespace rz {
 enum class STATUS {
@@ -11,8 +13,10 @@ enum class STATUS {
   RZ_SUCCESS,
 };
 
-using logCB = std::function<void(const std::string &)>;
-inline constexpr auto emptyCB = [](const std::string &) {};
+using logCB = std::function<void(std::string_view)>;
+inline constexpr auto emptyCB = [](std::string_view) {};
+inline constexpr auto stdoutCB = [](std::string_view _log) { std::println("{}", _log); };
+inline constexpr auto stderrCB = [](std::string_view _log) { std::println(stderr, "{}", _log); };
 
 template <typename T>
 using EXPECTED_UPTR = std::expected<std::unique_ptr<T>, STATUS>;
--- tests/test_fs.cpp
+++ tests/test_fs.cpp
@@ -2,15 +2,11 @@
 
 #include <rz/fs/fs.hpp>
 #include <rz/defs/defs.hpp>
-#include <string>
-#include <iostream>
 
 TEST(FS, GetAppConfigPath)
 {
     using namespace rz;
 
-    auto res = fs::GetAppConfigPath("test_app", [](const std::string& log){
-        std::cerr << log << std::endl;
-    });
+    auto res = fs::GetAppConfigPath("test_app", stderrCB);
     EXPECT_TRUE(res.has_value());
 }
